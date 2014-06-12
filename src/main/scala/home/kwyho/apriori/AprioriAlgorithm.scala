0526d988ed1c10bde0817c8d4f5f69b5ec1137ae
Kwan-Yuet Ho, Ph.D <stephenhky@yahoo.com.hk>
2014-06-12 15:46:16 -0400

    adding elements to class

--- src/main/scala/home/kwyho/apriori/AprioriAlgorithm.scala
+++ src/main/scala/home/kwyho/apriori/AprioriAlgorithm.scala
@@ -5,12 +5,20 @@
 package home.kwyho.apriori
 
 import scala.io.Source
+import scala.collection.immutable.List
+import scala.collection.immutable.Set
+import java.io.File
 
-class AprioriAlgorithm(inputFileName: String) {
-  //val transactions : List[Set[String]]
-  //val itemSet : Set[String]
-  Source.fromFile(inputFileName).foreach {
-    print
+class AprioriAlgorithm(inputFile: File) {
+  var transactions : List[Set[String]] = List()
+  var itemSet : Set[String] = Set()
+
+  for (line<-Source.fromFile(inputFile).getLines()) {
+    val elementSet = line.trim.split(',').toSet
+    if (elementSet.size > 0) {
+      transactions = transactions :+ elementSet
+      itemSet = itemSet ++ elementSet
+    }
   }
 }
 
